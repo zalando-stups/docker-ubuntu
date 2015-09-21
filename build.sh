eab72e47c212bb689403facfa5abe3239126e79c
Ilari Mäkelä <ilari.makela@spoonfactory.fi>
2015-09-21 13:07:56 +0300

    Refactored code to be more readable

--- build.sh
+++ build.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-VER=$(cat VERSION)
+VER=$(grep FROM Dockerfile | awk -F':' '{print $2}')
 
 docker build --no-cache=true -t zalando/ubuntu:$VER .
 
 SQUASH_PATH=$(which docker-squash)
 
-if [ -z "$SQUASH_PATH" ]; then
+if [ -z "$SQUASH_PATH" ]
+then
     # install Docker Squash from https://github.com/jwilder/docker-squash
     TGZ=docker-squash-linux-amd64-v0.1.0.tar.gz
-    wget https://github.com/jwilder/docker-squash/releases/download/v0.1.0/$TGZ
-    sudo tar -C /usr/local/bin -xzvf $TGZ
-    rm $TGZ
+    curl -L -s https://github.com/jwilder/docker-squash/releases/download/v0.1.0/$TGZ | tar xzvf - -C /usr/local/bin
     SQUASH_PATH=$(which docker-squash)
 fi
 
 echo 'Squashing the base image to save space..'
 docker save zalando/ubuntu:$VER | sudo "$SQUASH_PATH" -from root -t zalando/ubuntu:$VER | docker load
+

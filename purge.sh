a776a7e5299ba521edae79a8ec4a252bdac66388
Henning Jacobs <henning@jacobs1.de>
2016-03-09 16:30:30 +0100

    mention docker-squash

--- purge.sh
+++ purge.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # remove assets which are not needed in production
+# this only makes sense when "docker-squash" is used afterwards!
 rm -fr /usr/share/doc/* /usr/share/man/* /usr/share/locale/?? /usr/share/locale/??_?? \
        /usr/share/icons/* /usr/share/sounds/* \
        /var/lib/apt/* \
